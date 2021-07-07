738771b5b78c6e217ffb4674586fa354ccb3e4ac
Alexey Frolov <a.frolov@systtech.com>
2021-07-07 23:09:49 +0300

    facade

---
+++ Facade/Classic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class Classic
+    {
+        public static void Run()
+        {
+            var lib1 = new Library1();
+            var lib2 = new Library2();
+            var facade = new Facade(lib1, lib2);
+            Console.WriteLine(facade.Operation());
+        }
+    }
+
+    public class Library1
+    {
+        public string Operation()
+        {
+            return "Operation Library1";
+        }
+    }
+
+    public class Library2
+    {
+        public string Operation()
+        {
+            return "Operation Library2";
+        }
+    }
+
+    public class Facade
+    {
+        private readonly Library1 library1;
+        private readonly Library2 library2;
+
+        public Facade(Library1 library1, Library2 library2)
+        {
+            this.library1 = library1;
+            this.library2 = library2;
+        }
+
+        public string Operation()
+        {
+            return library1.Operation() + Environment.NewLine + library2.Operation();
+        }
+    }
+}
